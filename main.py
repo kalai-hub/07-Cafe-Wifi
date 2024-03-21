224eb5754412593368d97a502418449c2453ed51
Kalai <k.kalaiarasanwelcome@gmail.com>
2024-03-22 00:58:38 +0530

    Adding login page

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class Base(DeclarativeBase):
 
 # Connect to Database
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///cafes.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
